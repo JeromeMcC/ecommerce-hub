86a049743d039e0e72f704c4c87f7bf732c2a6b3
Jerome McClarin <Jerome.Mcclarin@gmail.com>
2022-03-29 17:42:23 -0400

    category route done

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -5,23 +5,53 @@ const { Category, Product } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all categories
+    Category.findAll({
+      include:[Product],
+    })
+    .then((catagories) => res.json(catagories))
+    .catch((err) =>  res.status(500).json(err));
   // be sure to include its associated Products
 });
 
 router.get('/:id', (req, res) => {
+    Category.findOne({
+      where: {
+        id:req.params.id,
+      },
+      include: [Product],
+      })
+     .then((catagory => res.json(catagory)))
+     .catch((err) => res.status(500).json(err));
   // find one category by its `id` value
   // be sure to include its associated Products
 });
 
 router.post('/', (req, res) => {
+  Category.create(req.body)
+  .then((category) => res.status(200).json(catagory))
+  .catch((err) => res.status(400).json(err));
   // create a new category
 });
 
 router.put('/:id', (req, res) => {
+  Category.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((catagory) => res.status(200).json(catagory))
+    .catch((err)=> res.status(400).json(err));
   // update a category by its `id` value
 });
 
 router.delete('/:id', (req, res) => {
+  Category.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+  .then((category) => res.status(200).json(category))
+  .catch((err) => res.status(400).json(err));
   // delete a category by its `id` value
 });
 
